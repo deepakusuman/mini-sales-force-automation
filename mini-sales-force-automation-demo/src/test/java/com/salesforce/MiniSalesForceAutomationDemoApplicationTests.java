855ba8a9276bd8e6e68c4fa24d12a58dda972883
Deepa Kusuman <65413714+deepakusuman@users.noreply.github.com>
2021-11-08 10:35:50 +0530

    Add files via upload

---
+++ mini-sales-force-automation-demo/src/test/java/com/salesforce/MiniSalesForceAutomationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSalesForceAutomationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
